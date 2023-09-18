fc11d76e38fc8638763fb60e655e1c8bf0f816b7
jokandro <114486039+jokandro@users.noreply.github.com>
2023-09-18 16:20:34 +0800

    feat: insert, delete,find,traversals,height, depth check if balanced, rebalance, test cases

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -3,9 +3,9 @@ const Node = (data, left = null, right = null) => {
 };
 
 const Tree = (array) => {
-  const root = buildTree(array);
+  let root = buildTree(array);
 
-  const prettyPrint = (node, prefix = "", isLeft = true) => {
+  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
     if (node === null) {
       return;
     }
@@ -21,13 +21,13 @@ const Tree = (array) => {
   const insert = (value) => {
     let nodeToInsert = Node(value);
     if (root == null) {
-      return root= nodeToInsert;
+      return (root = nodeToInsert);
     }
     let prev = null;
     let temp = root;
     while (temp != null) {
-      if(temp.data == value){
-        return
+      if (temp.data == value) {
+        return;
       }
 
       if (temp.data > value) {
@@ -39,13 +39,212 @@ const Tree = (array) => {
       }
     }
     if (prev.data > value) {
-       (prev.left = nodeToInsert);
+      prev.left = nodeToInsert;
     } else {
-       (prev.right = nodeToInsert);
+      prev.right = nodeToInsert;
     }
   };
 
-  return { root, prettyPrint, insert };
+  const deleteNode = (root, dataToDelete) => {
+    if (root == null) return root;
+
+    if (root.data > dataToDelete) {
+      root.left = deleteNode(root.left, dataToDelete);
+      return root;
+    } else if (root.data < dataToDelete) {
+      root.right = deleteNode(root.right, dataToDelete);
+      return root;
+    }
+
+    if (root.left == null) {
+      let temp = root.right;
+      delete root;
+      return temp;
+    } else if (root.right == null) {
+      let temp = root.left;
+      delete root;
+      return temp;
+    } else {
+      let succeedingParent = root;
+
+      let successor = root.right;
+      while (successor.left != null) {
+        succeedingParent = successor;
+        successor = successor.left;
+      }
+
+      if (succeedingParent !== root) {
+        succeedingParent.left = successor.right;
+      } else {
+        succeedingParent.right = successor.right;
+      }
+
+      root.data = successor.data;
+
+      delete successor;
+      return root;
+    }
+  };
+
+  const find = (root, data) => {
+    if (root == null || root.data == data) {
+      return root;
+    }
+
+    if (root.data < data) {
+      return find(root.right, data);
+    }
+
+    return find(root.left, data);
+  };
+
+  const levelOrder = () => {
+    if (root == null) return;
+
+    let queue = [];
+    queue.push(root);
+    let current;
+    let outputText = "Level Order:";
+    while (!queue.length == 0) {
+      current = queue[0];
+      // console.log(current.data);
+      outputText += ` ${current.data}`;
+      if (current.left != null) {
+        queue.push(current.left);
+      }
+      if (current.right != null) {
+        queue.push(current.right);
+      }
+      queue.shift();
+    }
+    return outputText;
+  };
+
+  const preorder = (root) => {
+    let text = "Preorder:";
+    const preorderAlgorithm = (root) => {
+      if (root == null) return;
+
+      text += ` ${root.data}`;
+      preorderAlgorithm(root.right);
+      preorderAlgorithm(root.left);
+      return text;
+    };
+
+    return preorderAlgorithm(root);
+  };
+
+  const inorder = (root) => {
+    let text = "Inorder:";
+    const inorderAlgorithm = (root) => {
+      if (root == null) return;
+      inorderAlgorithm(root.left);
+      text += ` ${root.data}`;
+      inorderAlgorithm(root.right);
+      return text;
+    };
+    return inorderAlgorithm(root);
+  };
+
+  const postorder = (root) => {
+    let text = "Postorder:";
+    const postorderAlgorithm = (root) => {
+      if (root == null) return;
+      postorderAlgorithm(root.left);
+      postorderAlgorithm(root.right);
+      text += ` ${root.data}`;
+      return text;
+    };
+    return postorderAlgorithm(root);
+  };
+
+  const height = (nodeData) => {
+    const node = find(root, nodeData);
+
+    const findHeight = (node) => {
+      if (node == null) return -1;
+
+      const leftHeight = findHeight(node.left);
+      const rightHeight = findHeight(node.right);
+
+      if (leftHeight > rightHeight) {
+        return leftHeight + 1;
+      } else {
+        return rightHeight + 1;
+      }
+    };
+
+    const printResult = () => {
+      if (node == null) return `Given node(${nodeData}) doesnt exist`;
+
+      return `node(${nodeData}) height is ${findHeight(node)}`;
+    };
+
+    return printResult();
+  };
+
+  const depth = (nodeData, currentNode = root) => {
+    if (currentNode == null) return;
+    if (nodeData == currentNode.data) return 0;
+    if (nodeData < currentNode.data)
+      return depth(nodeData, currentNode.left) + 1;
+    if (nodeData > currentNode.data)
+      return depth(nodeData, currentNode.right) + 1;
+  };
+
+  const isBalanced = () => {
+    const height = (root) => {
+      if (root == null) return 0;
+      return Math.max(height(root.left), height(root.right)) + 1;
+    };
+
+    const checkBalance = (root) => {
+      if (root == null) return true;
+
+      let lh = height(root.left);
+      let rh = height(root.right);
+
+      if (
+        Math.abs(lh - rh) <= 1 &&
+        checkBalance(root.left) == true &&
+        checkBalance(root.right) == true
+      )
+        return "balanced: true";
+
+      return "balanced: false";
+    };
+
+    return console.log(checkBalance(root));
+  };
+
+  const rebalance = () => {
+    let array = [];
+    const inorderAlgorithm = (root) => {
+      if (root == null) return;
+      inorderAlgorithm(root.left);
+      array.push(root.data);
+      inorderAlgorithm(root.right);
+      return array;
+    };
+    const inorderArray = inorderAlgorithm(root);
+    root = buildTree(inorderArray);
+  };
+
+  return {
+    root,
+    prettyPrint,
+    insert,
+    deleteNode,
+    find,
+    levelOrder,
+    inorder,
+    postorder,
+    preorder,
+    height,
+    depth,
+    isBalanced,
+    rebalance,
+  };
 };
 
 const buildTree = (array) => {
@@ -68,9 +267,41 @@ const buildTree = (array) => {
   return rootNode;
 };
 
-const testBST = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-testBST.prettyPrint(testBST.root);
-testBST.insert(0.1);
-(testBST.prettyPrint(testBST.root));
-// const test = Tree('a')
-// console.log(testBST.insert('a'))
+const randomArrayGenerator = (n = 10) => {
+  let array = [];
+  for (i = 0; i <= n; i++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+
+  return array;
+};
+
+const printElements = () => {
+  console.log(testBST.levelOrder());
+  console.log(testBST.preorder(testBST.root));
+  console.log(testBST.inorder(testBST.root));
+  console.log(testBST.postorder(testBST.root));
+};
+
+const insertRandomNumbersToTree = (n = 10,tree) => {
+  for (let index = 0; index < n; index++) {
+    tree.insert(Math.floor(Math.random() * 1000));
+  }
+};
+
+const testArray = randomArrayGenerator();
+const testBST = Tree(testArray);
+
+testBST.prettyPrint();
+testBST.isBalanced();
+printElements();
+// console.log(testBST)
+insertRandomNumbersToTree(5,testBST);
+testBST.prettyPrint();
+testBST.isBalanced();
+printElements();
+
+testBST.rebalance();
+testBST.prettyPrint();
+testBST.isBalanced();
+printElements();
